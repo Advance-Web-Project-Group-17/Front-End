9c9e3f4e2bfb0620cdd2bea6fc25213a4e697874
Tam309 <t3doch00@students.oamk.fi>
2024-11-12 16:01:16 +0200

    test api and demo home page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import HomePage from "./Components/HomePage"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
