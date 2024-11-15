7667e60473c01f60463a3b7b7a5d9f7d65ab0851
bmQ88rk4Ry?3c?iC <t3doch00@students.oamk.fi>
2024-11-15 15:12:35 +0200

    update fetch data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import LandingPage from './Screen/LandingPage/LandingPage.jsx';
+import './App.css';
 
 function App() {
-  
+  const [movies, setMovies] = useState([]);
+  const [genres, setGenres] = useState({});
+
+  // Fetch movie data from TMDB API
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const apiKey = "01317230958149c57ec3ae676ea0850a";
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`
+        );
+        
+        // Map genre IDs to genre names
+        const moviesData = response.data.results.map((movie) => ({
+          id: movie.id,
+          title: movie.title,
+          image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+          slidingImage: `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`,
+          rating: movie.vote_average,
+          genres: movie.genre_ids.map((id) => genres[id] || 'Unknown'), // Map genre IDs to genre names
+          synopsis: movie.overview,
+        }));
+        setMovies(moviesData);
+      } catch (e) {
+        console.error("Error fetching movies:", e);
+      }
+    };
+
+    fetchMovies();
+  }, [genres]); // Depend on genres to ensure movies data updates once genres are loaded
+
+  // Fetch genre data from TMDB API
+  useEffect(() => {
+    const fetchGenres = async () => {
+      try {
+        const url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
+        const response = await axios.get(url, {
+          headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTMxNzIzMDk1ODE0OWM1N2VjM2FlNjc2ZWEwODUwYSIsIm5iZiI6MTczMTY2OTcxMC45MTAzNzQyLCJzdWIiOiI2NzMzMjdhZjFkNzJlYjY1ZjQyZTY5ZmEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.v7xAB7bBiSPC3axkDHPTrxRG9aDzg5E6b0oBfnG0TDs'
+          }
+        });
+        
+        // Create a mapping of genre ID to genre name
+        const genreMap = response.data.genres.reduce((acc, genre) => {
+          acc[genre.id] = genre.name;
+          return acc;
+        }, {});
+        
+        setGenres(genreMap);
+      } catch (error) {
+        console.error('Error fetching genres:', error);
+      }
+    };
+
+    fetchGenres();
+  }, []);
+
   return (
     <div className="App">
-      <LandingPage />
+      <LandingPage movies={movies} genres={genres} />
     </div>
   );
 }
