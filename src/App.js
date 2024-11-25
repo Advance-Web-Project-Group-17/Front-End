3727e11b184a908ac10c2d99585e58f9c09e8ebd
Tam309 <t3doch00@students.oamk.fi>
2024-11-25 13:08:18 +0200

    fetch genre for tv show

--- src/App.js
+++ src/App.js
@@ -6,31 +6,68 @@ import LoginPage from "./Screen/LoginPage/LoginPage.jsx";
 import RegisterPage from "./Screen/RegisterPage/RegisterPage.jsx";
 import Navbar from "./Screen/NavBar/NavBar.jsx";
 import MoviesPage from "./Screen/MoviesPage/MoviesPage.jsx";
+import ShowTimesPage from "./Screen/ShowTimesPage/ShowTimesPage.jsx";
 import './App.css';
 
 function App() {
   const [trendingMovies, setTrendingMovies] = useState([]);
-  const [movies, SetMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
   const [genres, setGenres] = useState({});
   const [isLoggedIn, setIsLoggedIn] = useState(sessionStorage.getItem("isLoggedIn") === "true");
 
-  // Fetch movie data from TMDB API
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const tmdbHeaders = {
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTMxNzIzMDk1ODE0OWM1N2VjM2FlNjc2ZWEwODUwYSIsIm5iZiI6MTczMTY2OTcxMC45MTAzNzQyLCJzdWIiOiI2NzMzMjdhZjFkNzJlYjY1ZjQyZTY5ZmEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.v7xAB7bBiSPC3axkDHPTrxRG9aDzg5E6b0oBfnG0TDs'
+    }
+  };
+
+  // Fetch movie and TV genres and combine them
+  useEffect(() => {
+    const fetchCombinedGenres = async () => {
+      try {
+        const [movieGenresResponse, tvGenresResponse] = await Promise.all([
+          axios.get('https://api.themoviedb.org/3/genre/movie/list?language=en', tmdbHeaders),
+          axios.get('https://api.themoviedb.org/3/genre/tv/list?language=en', tmdbHeaders)
+        ]);
+
+        // Combine movie and TV genres into one object
+        const combinedGenres = {
+          ...movieGenresResponse.data.genres.reduce((acc, genre) => {
+            acc[genre.id] = genre.name;
+            return acc;
+          }, {}),
+          ...tvGenresResponse.data.genres.reduce((acc, genre) => {
+            acc[genre.id] = genre.name;
+            return acc;
+          }, {})
+        };
+
+        setGenres(combinedGenres);
+      } catch (error) {
+        console.error('Error fetching genres:', error);
+      }
+    };
+
+    fetchCombinedGenres();
+  }, []);
+
+  // Fetch trending movies
   useEffect(() => {
     const fetchTrendingMovies = async () => {
       try {
-        const apiKey = "01317230958149c57ec3ae676ea0850a";
         const response = await axios.get(
           `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`
         );
-        
-        // Map genre IDs to genre names
+
         const moviesData = response.data.results.map((movie) => ({
           id: movie.id,
           title: movie.title,
           image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
           slidingImage: `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`,
           rating: movie.vote_average.toFixed(1),
-          genres: movie.genre_ids.map((id) => genres[id] || 'Unknown'), // Map genre IDs to genre names
+          genres: movie.genre_ids.map((id) => genres[id] || 'Unknown'),
           synopsis: movie.overview,
         }));
         setTrendingMovies(moviesData);
@@ -39,69 +76,46 @@ function App() {
       }
     };
 
-    fetchTrendingMovies();
-  }, [genres]); // Depend on genres to ensure movies data updates once genres are loaded
+    if (Object.keys(genres).length > 0) {
+      fetchTrendingMovies();
+    }
+  }, [genres]);
 
+  // Fetch movies
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        const apiKey = "01317230958149c57ec3ae676ea0850a";
         const response = await axios.get(
           `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`
         );
-        
-        // Map genre IDs to genre names
+
         const moviesData = response.data.results.map((movie) => ({
           id: movie.id,
           title: movie.title,
           image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
           rating: movie.vote_average.toFixed(1),
-          genres: movie.genre_ids.map((id) => genres[id] || 'Unknown'), // Map genre IDs to genre names
+          genres: movie.genre_ids.map((id) => genres[id] || 'Unknown'),
           synopsis: movie.overview,
         }));
-        SetMovies(moviesData);
+        setMovies(moviesData);
       } catch (e) {
         console.error("Error fetching movies:", e);
       }
     };
 
-    fetchMovies();
+    if (Object.keys(genres).length > 0) {
+      fetchMovies();
+    }
   }, [genres]);
-
-  // Fetch genre data from TMDB API
-  useEffect(() => {
-    const fetchGenres = async () => {
-      try {
-        const url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
-        const response = await axios.get(url, {
-          headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTMxNzIzMDk1ODE0OWM1N2VjM2FlNjc2ZWEwODUwYSIsIm5iZiI6MTczMTY2OTcxMC45MTAzNzQyLCJzdWIiOiI2NzMzMjdhZjFkNzJlYjY1ZjQyZTY5ZmEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.v7xAB7bBiSPC3axkDHPTrxRG9aDzg5E6b0oBfnG0TDs'
-          }
-        });
-        
-        // Create a mapping of genre ID to genre name
-        const genreMap = response.data.genres.reduce((acc, genre) => {
-          acc[genre.id] = genre.name;
-          return acc;
-        }, {});
-        
-        setGenres(genreMap);
-      } catch (error) {
-        console.error('Error fetching genres:', error);
-      }
-    };
-
-    fetchGenres();
-  }, []);
 
   return (
     <div className="App">
       <Router>
-      <Navbar isLoggedIn={isLoggedIn}/>
+        <Navbar isLoggedIn={isLoggedIn} />
         <Routes>
-          <Route path="/" element={<LandingPage movies={trendingMovies} />} /> 
-          <Route path="/movies" element={<MoviesPage movies={movies} />} /> 
+          <Route path="/" element={<LandingPage movies={trendingMovies} />} />
+          <Route path="/movies" element={<MoviesPage movies={movies} />} />
+          <Route path="/show" element={<ShowTimesPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
