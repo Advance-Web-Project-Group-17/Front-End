b88c1fd471c3e6a4cbed04020c174c957681d290
Tam309 <t3doch00@students.oamk.fi>
2024-11-28 12:22:18 +0200

    minor fix

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ import TVShowGrid from "./Screen/TvshowPage/TVShowGrid.jsx";
 
 function App() {
   const [trendingMovies, setTrendingMovies] = useState([]);
-  const [movies, SetMovies] = useState([]);
-  const [trendingTVShows, setTrendingTVShows] = useState([]);
+  const [movies, setMovies] = useState([]);
   const [tvShows, setTVShows] = useState([]);
   const [genres, setGenres] = useState({});
   const [isLoggedIn, setIsLoggedIn] = useState(sessionStorage.getItem("isLoggedIn") === "true");
@@ -115,39 +114,38 @@ function App() {
   }, [genres]);
 
 // Fetch trending TV shows from TMDB API
-useEffect(() => {
-  const fetchTrendingTVShows = async () => {
-    try {
-      const apiKey = "01317230958149c57ec3ae676ea0850a";
-      const response = await axios.get(
-        `https://api.themoviedb.org/3/trending/tv/day?api_key=${apiKey}`
-      );
+// useEffect(() => {
+//   const fetchTrendingTVShows = async () => {
+//     try {
+//       const apiKey = "01317230958149c57ec3ae676ea0850a";
+//       const response = await axios.get(
+//         `https://api.themoviedb.org/3/trending/tv/day?api_key=${apiKey}`
+//       );
 
-      // Map genre IDs to genre names
-      const tvShowsData = response.data.results.map((tvShow) => ({
-        id: tvShow.id,
-        title: tvShow.name, // Use 'name' for TV shows instead of 'title'
-        image: `https://image.tmdb.org/t/p/w500${tvShow.poster_path}`,
-        slidingImage: `https://image.tmdb.org/t/p/w1280${tvShow.backdrop_path}`,
-        rating: tvShow.vote_average.toFixed(1),
-        genres: tvShow.genre_ids.map((id) => genres[id] || 'Unknown'), // Map genre IDs to genre names
-        synopsis: tvShow.overview,
-      }));
-      setTrendingTVShows(tvShowsData);
-    } catch (e) {
-      console.error("Error fetching TV shows:", e);
-    }
-  };
+//       // Map genre IDs to genre names
+//       const tvShowsData = response.data.results.map((tvShow) => ({
+//         id: tvShow.id,
+//         title: tvShow.name, // Use 'name' for TV shows instead of 'title'
+//         image: `https://image.tmdb.org/t/p/w500${tvShow.poster_path}`,
+//         slidingImage: `https://image.tmdb.org/t/p/w1280${tvShow.backdrop_path}`,
+//         rating: tvShow.vote_average.toFixed(1),
+//         genres: tvShow.genre_ids.map((id) => genres[id] || 'Unknown'), // Map genre IDs to genre names
+//         synopsis: tvShow.overview,
+//       }));
+//       setTrendingTVShows(tvShowsData);
+//     } catch (e) {
+//       console.error("Error fetching TV shows:", e);
+//     }
+//   };
 
-  fetchTrendingTVShows();
-}, [genres]); // Depend on genres to ensure TV shows data updates once genres are loaded
+//   fetchTrendingTVShows();
+// }, [genres]); // Depend on genres to ensure TV shows data updates once genres are loaded
 
 useEffect(() => {
   const fetchTVShows = async () => {
     try {
-      const apiKey = "01317230958149c57ec3ae676ea0850a";
       const response = await axios.get(
-        `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}`
+        `https://api.themoviedb.org/3/discover/tv`,tmdbHeaders
       );
 
       // Map genre IDs to genre names
@@ -159,6 +157,7 @@ useEffect(() => {
         genres: tvShow.genre_ids.map((id) => genres[id] || 'Unknown'), // Map genre IDs to genre names
         synopsis: tvShow.overview,
       }));
+      console.log(tvShowsData)
       setTVShows(tvShowsData);
     } catch (e) {
       console.error("Error fetching TV shows:", e);
@@ -180,6 +179,7 @@ useEffect(() => {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/profile" element={<ProfilePage setIsLoggedIn={setIsLoggedIn}/>} />
           <Route path="/edit" element={<EditProfilePage />} />
+          <Route path="/tvshows" element={<TVShowGrid movies={tvShows}/>} />
         </Routes>
       </Router>
     </div>
