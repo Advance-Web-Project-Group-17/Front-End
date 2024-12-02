ac8afe6a3f70480489689e647b221e7c8a5c907d
mohiuddinislam <t3ismd02@students.oamk.fi>
2024-12-02 19:58:26 +0200

    adding profie list

---
+++ src/Screen/ProfileList/Profilelist.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Profilelist() {
+  return (
+    <div>
+
+
+<body class="bg-gray-900 flex items-center justify-center min-h-screen">
+    <div class="border-4 border-blue-400 p-4">
+        <div class="bg-gray-800 text-white text-center py-2 rounded-md mb-4">
+            <h1 class="text-2xl font-fancy">Profile List</h1>
+        </div>
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+            <div class="bg-gray-400 h-24 rounded-md"></div>
+            <div class="bg-gray-400 h-24 rounded-md"></div>
+            <div class="bg-gray-400 h-24 rounded-md"></div>
+            <div class="bg-gray-400 h-24 rounded-md"></div>
+            <div class="bg-gray-400 h-24 rounded-md"></div>
+            <div class="bg-gray-400 h-24 rounded-md"></div>
+            <div class="bg-gray-400 h-24 rounded-md"></div>
+            <div class="bg-gray-400 h-24 rounded-md"></div>
+        </div>
+    </div>
+</body>
+
+
+        
+    </div>
+  )
+}
